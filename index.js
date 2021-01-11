90b9249a2340a9210eb32f8755f11db27dbc06a7
Tanvi Aditya <tanviaditya1@gmail.com>
2021-01-11 15:45:09 +0530

    First

--- index.js
+++ index.js
@@ -5,27 +5,51 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = 3000
-
+var listofuser=[];
 
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+//http end points
+app.get('/quick-chat/' , (req, res) => {
+    res.redirect(`/quick-chat/${uuid.v4()}`)
+})
+
 app.get('/quick-chat/new', (req, res) => {
     res.redirect(`/quick-chat/${uuid.v4()}`)
 })
+
 app.get('/quick-chat/:room', (req, res) =>{
     res.render('chatroom', {room: req.params.room})
 })
+
 io.on('connection', (socket) =>{
-    socket.on('join', ({room}) =>{
+    socket.on('join', ({room,name}) =>{
+        listofuser.push(name)
+        console.log(listofuser)
+        socket.to(room).emit("joined_room",{name})
         socket.join(room)
+
+        //update users
+        updateUsers(socket);
+        
     });
     socket.on('chat_message', (payload) =>{
         socket.to(payload.room).emit("chat_message", payload);
     })
+    // image upload
    })
+   function updateUsers(socket){
+    // Make a list of all the clients nicknames
+    nicknames = [];
+    for (i=0 ; i<listofuser.length ; i++){
+        nicknames.push(listofuser[i]);
+    }
 
+    // Tell all the clients which clients are currently in the chat-room
+    socket.emit('update current users' , nicknames);
+}
 
 http.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
